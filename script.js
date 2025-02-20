41851f6445dce176be634dbff4bcbb6c5e78c20e
anyamaniv <108199782+anyamaniv@users.noreply.github.com>
2025-02-20 10:55:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+// Function to make the website responsive
+function adjustLayout() {
+    let screenWidth = window.innerWidth;
+
+    if (screenWidth < 768) {
+        document.querySelector(".tour-banner").style.height = "auto";
+        document.querySelector(".tour-banner img").style.width = "100%";
+        document.querySelector(".tour-banner img").style.objectFit = "cover";
+    } else {
+        document.querySelector(".tour-banner").style.height = "100vh";
+    }
+}
+
+// Function to detect browser
+function detectBrowser() {
+    let userAgent = navigator.userAgent;
+
+    if (userAgent.includes("Safari") && !userAgent.includes("Chrome")) {
+        document.querySelector(".tour-banner img").style.objectFit = "contain";
+    } else if (userAgent.includes("Firefox")) {
+        document.querySelector("body").style.fontSize = "1.1rem";
+    }
+}
+
+// Run functions on page load and window resize
+window.onload = function () {
+    adjustLayout();
+    detectBrowser();
+};
+
+window.onresize = adjustLayout;
 
